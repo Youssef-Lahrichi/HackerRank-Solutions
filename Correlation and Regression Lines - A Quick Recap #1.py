84c1d3a62c0124bcaeb807804e90d6e9253de103
Youssef Lahrichi <52592818+Youssef-Lahrichi@users.noreply.github.com>
2022-05-03 23:25:39 -0700

    Update Correlation and Regression Lines - A Quick Recap #1.py
    fixed divide by zero issue

--- Correlation and Regression Lines - A Quick Recap #1.py
+++ Correlation and Regression Lines - A Quick Recap #1.py
@@ -1,11 +1,7 @@
 import math
 
-x = [15,  12,  8,   8,   7,   7,   7,   6,   5,   3]
-y = [10,  25,  17,  11,  13,  17,  20,  13,  9,   15]
-
 x_mean = float(sum(x)/len(x))
 y_mean = float(sum(y)/len(y))
-
 
 numer, denom1, denom2 = 0, 0, 0
 
@@ -14,5 +10,6 @@ for i in range(len(x)):
     denom1 += (x[i] - x_mean)**2
     denom2 += (y[i] - y_mean)**2
 
-r = numer / (math.sqrt(denom1)*math.sqrt(denom2))
+denom = (math.sqrt(denom1)*math.sqrt(denom2))
+return numer / denom if denom != 0 else 0
 print(r)
